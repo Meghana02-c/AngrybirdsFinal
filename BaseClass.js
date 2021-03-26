f2c316ff8e432b2225f4389031b1df3daaa96e11
Meghana02-c <74354176+Meghana02-c@users.noreply.github.com>
2021-03-27 00:08:39 +0530

    Update BaseClass.js

--- BaseClass.js
+++ BaseClass.js
@@ -3,7 +3,7 @@ class BaseClass{
         var options = {
             'restitution':0.5,
             'friction':1,
-            'density':0.8
+            'density':1
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
